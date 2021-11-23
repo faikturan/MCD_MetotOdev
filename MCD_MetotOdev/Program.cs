03e2d0c53652e1ade65037e6df4da6dba13ee7a8
Faik Turan <turanfaik@hotmail.com>
2021-11-23 18:22:51 +0300

    changed metod

--- MCD_MetotOdev/Program.cs
+++ MCD_MetotOdev/Program.cs
@@ -30,6 +30,53 @@ namespace MCD_MetotOdev
              * 
              */
             #endregion
+
+
+            Matematik M = new Matematik();
+            yenidenIslemYap:
+            M.menuHazirla();
+            int kullaniciSecim = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 1. sayı değerini giriniz: ");
+            decimal kullaniciSayi1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. sayı değerini giriniz: ");
+            decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch (kullaniciSecim)
+            {
+                case 1://Toplama
+                    sonuc = M.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "+");
+                    break;
+                case 2://Cikartma
+                    sonuc = M.cikartmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "-");
+                    break;
+                case 3://Carpma
+                    sonuc = M.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                    break;
+                case 4://Bölme
+                    sonuc = M.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                    break;
+                default:
+                    Console.WriteLine("Belirtmiş olduğunuz değer liste içerisinde bulunamadı.");
+                    Console.WriteLine("Lütfen tekrar deneyiniz.");
+                    System.Threading.Thread.Sleep(2000);
+                    goto yenidenIslemYap;
+            }
+
+            Console.WriteLine("Yeni bir işlem yapmak istiyor musunuz?(E/H)");
+            string EH = Console.ReadLine();
+            if (EH == "E")
+            {
+                goto yenidenIslemYap;
+            }
+
         }
     }
 }
