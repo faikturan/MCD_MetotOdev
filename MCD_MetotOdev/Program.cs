a531345e351bbc4ce25313e217e80989f131e934
Faik Turan <turanfaik@hotmail.com>
2021-11-23 17:32:48 +0300

    Matematik class and methods add

--- MCD_MetotOdev/Program.cs
+++ MCD_MetotOdev/Program.cs
@@ -10,7 +10,26 @@ namespace MCD_MetotOdev
     {
         static void Main(string[] args)
         {
-
+            #region 
+            /*
+             * Matematik adında bir class oluşturun.
+             * bir menu hazırlayın metot ile 
+             * menüde
+             * "****Menü****"
+             * "1 - Toplama "
+             * "2 - Çıkartma "
+             * "3 - Çarpma
+             * "4 - Bölme
+             * "Lütfen bir değer seçiniz:"
+             * Metot ile 4 işlemleri ekrana yazdır metot ile yazdırılacak
+             * public static void içinde
+             * Matematik sınıfının bir instance'ini al 
+             * Kullanıcıdan 2 değer iste ve Matemetik sınıfındaki 4 işlem metodunu kullanarak
+             * sonuçları ekrana yazdır. switvh case
+             * Yeniden işlem yapmak istiyormusunuz? (E/H) sorunuz.
+             * 
+             */
+            #endregion
         }
     }
 }
